1bfb3e30abf6af396b0d73c02ed4c1b7e36aa774
jirachiwish <40514888+jirachiwish@users.noreply.github.com>
2021-01-01 18:11:37 +0800

    Update strings.h

--- include/strings.h
+++ include/strings.h
@@ -1014,6 +1014,7 @@ extern const u8 gText_ThrowInPremierBall[];
 extern const u8 gText_ShopBuy[];
 extern const u8 gText_ShopSell[];
 extern const u8 gText_ShopQuit[];
+extern const u8 gText_SoldOut[];
 
 extern const u8 gText_OhABite[];
 extern const u8 gText_PokemonOnHook[];
