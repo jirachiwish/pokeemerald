82959ea04d795c74da45b7f29d3a9657895d6cd2
jirachiwish <40514888+jirachiwish@users.noreply.github.com>
2021-01-11 20:59:09 +0800

    Update metatile_behavior.c

--- src/metatile_behavior.c
+++ src/metatile_behavior.c
@@ -368,9 +368,9 @@ bool8 MetatileBehavior_IsEscalator(u8 metatileBehavior)
         return FALSE;
 }
 
-bool8 Unref_MetatileBehavior_IsUnused04(u8 metatileBehavior)
+bool8 MetatileBehavior_IsSnowGrass(u8 metatileBehavior)
 {
-    if (metatileBehavior == MB_UNUSED_04)
+    if (metatileBehavior == MB_SNOW_GRASS)
         return TRUE;
     else
         return FALSE;
